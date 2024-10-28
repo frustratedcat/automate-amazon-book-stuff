5df7b085c006ac29bc748816880ffa909ae12681
frustratedcat <brownjared205@gmail.com>
2024-10-28 19:13:15 -0400

    Sign into page

--- main.py
+++ main.py
@@ -8,18 +8,13 @@ driver = webdriver.Firefox()
 
 driver.get("https://wwww.amazon.com")
 
-# driver.implicitly_wait(5)
-
+driver.implicitly_wait(5)
 click_sign_in = driver.find_element(By.ID, "nav-link-accountList").click()
 
+driver.implicitly_wait(5)
 input_email = driver.find_element(By.ID, "ap_email")
 input_email.send_keys(creds.username + Keys.ENTER)
 
 driver.implicitly_wait(5)
-
 input_password = driver.find_element(By.ID, "ap_password")
 input_password.send_keys(creds.password + Keys.ENTER)
-
-time.sleep(5)
-
-driver.quit()
