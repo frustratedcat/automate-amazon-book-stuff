3d7d34627bb7aecf9f28adadbc7291940d11bb89
frustratedcat <brownjared205@gmail.com>
2024-10-28 17:26:19 -0400

    Update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+import creds
 
 driver = webdriver.Firefox()
 
@@ -12,10 +13,12 @@ driver.get("https://wwww.amazon.com")
 click_sign_in = driver.find_element(By.ID, "nav-link-accountList").click()
 
 input_email = driver.find_element(By.ID, "ap_email")
-input_email.send_keys("" + Keys.ENTER)
+input_email.send_keys(creds.username + Keys.ENTER)
+
+driver.implicitly_wait(5)
 
 input_password = driver.find_element(By.ID, "ap_password")
-input_password.send_keys("" + Keys.ENTER)
+input_password.send_keys(creds.password + Keys.ENTER)
 
 time.sleep(5)
 
