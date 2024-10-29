c5fb606a5e104b0cc588d82e1f52eae5fb668738
frustratedcat <brownjared205@gmail.com>
2024-10-28 20:03:36 -0400

    Add click for content library and change function name

--- main.py
+++ main.py
@@ -13,16 +13,17 @@ def click_account_list(time):
   implicitly_wait(time)
   account_list = driver.find_element(By.ID, "nav-link-accountList").click()
 
-def input_email_password(var, type, name, file):
+def input_select(var, type, name, file):
   implicitly_wait(5)
   var = driver.find_element(type, name)
-  var.send_keys(file + Keys.ENTER)
+
+  if file:
+    var.send_keys(file + Keys.ENTER)
+  else:
+    var.send_keys(Keys.ENTER)
 
 click_account_list(20)
-input_email_password('input_email', By.ID, 'ap_email', creds.username)
-input_email_password('input_password', By.ID, 'ap_password', creds.password)
+input_select('input_email', By.ID, 'ap_email', creds.username)
+input_select('input_password', By.ID, 'ap_password', creds.password)
 click_account_list(100)
-input_email_password('content_library', By.LINK_TEXT, 'Content Library', None)
-
-# content_library = driver.find_element(By.LINK_TEXT, 'Content Library')
-# content_library.send_keys(Keys.ENTER)
+input_select('content_library', By.LINK_TEXT, 'Content Library', None)
