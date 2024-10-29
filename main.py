ace34a093351bdf476b280b0430e14909f3daf13
frustratedcat <brownjared205@gmail.com>
2024-10-28 20:13:39 -0400

    Click books page and redo functions

--- main.py
+++ main.py
@@ -9,21 +9,22 @@ driver.get("https://wwww.amazon.com")
 def implicitly_wait(time):
   driver.implicitly_wait(time)
 
-def click_account_list(time):
+def click_select(time, var, type, name):
   implicitly_wait(time)
-  account_list = driver.find_element(By.ID, "nav-link-accountList").click()
+  var = driver.find_element(type, name).click()
 
-def input_select(var, type, name, file):
-  implicitly_wait(5)
+def input_select(time, var, type, name, file):
+  implicitly_wait(time)
   var = driver.find_element(type, name)
-
   if file:
     var.send_keys(file + Keys.ENTER)
   else:
     var.send_keys(Keys.ENTER)
 
-click_account_list(20)
-input_select('input_email', By.ID, 'ap_email', creds.username)
-input_select('input_password', By.ID, 'ap_password', creds.password)
-click_account_list(100)
-input_select('content_library', By.LINK_TEXT, 'Content Library', None)
+click_select(20, 'account_list', By.ID, 'nav-link-accountList')
+input_select(5, 'input_email', By.ID, 'ap_email', creds.username)
+input_select(5, 'input_password', By.ID, 'ap_password', creds.password)
+click_select(100, 'account_list', By.ID, 'nav-link-accountList')
+input_select(5, 'content_library', By.LINK_TEXT, 'Content Library', None)
+click_select(5, 'books', By.ID, 'content-ownership-books')
+
