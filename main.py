ce58a8cb39b104f0de32c20966cef01021a35509
frustratedcat <brownjared205@gmail.com>
2024-10-30 21:55:28 -0400

    Add name == main thing so real python devs dont get mad at me

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 import creds
 
 driver = webdriver.Firefox()
@@ -23,30 +21,33 @@ def input_select(time, var, type, name, file):
   else:
     var.send_keys(Keys.ENTER)
 
-# Log in
-click_select(20, 'account_list', By.ID, 'nav-link-accountList')
-input_select(5, 'input_email', By.ID, 'ap_email', creds.username)
-input_select(5, 'input_password', By.ID, 'ap_password', creds.password)
+def main():
+  # Log in
+  click_select(20, 'account_list', By.ID, 'nav-link-accountList')
+  input_select(5, 'input_email', By.ID, 'ap_email', creds.username)
+  input_select(5, 'input_password', By.ID, 'ap_password', creds.password)
 
-# Navigate to content library
-click_select(100, 'account_list', By.ID, 'nav-link-accountList')
-input_select(5, 'content_library', By.LINK_TEXT, 'Content Library', None)
-click_select(5, 'books', By.ID, 'content-ownership-books')
+  # Navigate to content library
+  click_select(100, 'account_list', By.ID, 'nav-link-accountList')
+  input_select(5, 'content_library', By.LINK_TEXT, 'Content Library', None)
+  click_select(5, 'books', By.ID, 'content-ownership-books')
 
-# Get pages and download items
-last_page = driver.find_element(By. XPATH, '//div[contains(@id, "pagination")]//a[last()]').text
+  # Get pages and download items
+  last_page = driver.find_element(By. XPATH, '//div[contains(@id, "pagination")]//a[last()]').text
 
-# Pages loop
-for num in range(0, int(last_page)):
-  # Next Page
-  click_select(20, 'page', By.XPATH, '//div[contains(@id, "pagination")]//a[contains(@id, "page-'+str(num+1)+'")]')
+  # Pages loop
+  for num in range(0, int(last_page)):
+    # Next Page
+    click_select(20, 'page', By.XPATH, '//div[contains(@id, "pagination")]//a[contains(@id, "page-'+str(num+1)+'")]')
 
-  all_books = driver.find_elements(By.XPATH, '//tr[contains(@class, "ListItem-module_row")]')
-  # Download items loop
-  for i in range(len(all_books)):
-    click_select(20, 'drop_down', By.XPATH, '//tr[contains(@class, "ListItem-module_row")]['+str(i + 1)+']//div[contains(@id, "dd_title")]')
-    click_select(20, 'transfer_click', By.XPATH, '//tr[contains(@class, "ListItem-module_row")]['+str(i + 1)+']//div[contains(@id, "DOWNLOAD_AND_TRANSFER")]')
-    click_select(20, 'click_device', By.XPATH, '//tr[contains(@class, "ListItem-module_row")]['+str(i + 1)+']//span[contains(@id, "download_and_transfer_list")]')
-    click_select(20, 'click_download', By.XPATH, '//tr[contains(@class, "ListItem-module_row")]['+str(i + 1)+']//div[contains(@id, "DOWNLOAD_AND_TRANSFER_ACTION")]/span[text()="Download"]')
-    click_select(20, 'notification_close', By.ID, 'notification-close')
+    all_books = driver.find_elements(By.XPATH, '//tr[contains(@class, "ListItem-module_row")]')
+    # Download items loop
+    for i in range(len(all_books)):
+      click_select(20, 'drop_down', By.XPATH, '//tr[contains(@class, "ListItem-module_row")]['+str(i + 1)+']//div[contains(@id, "dd_title")]')
+      click_select(20, 'transfer_click', By.XPATH, '//tr[contains(@class, "ListItem-module_row")]['+str(i + 1)+']//div[contains(@id, "DOWNLOAD_AND_TRANSFER")]')
+      click_select(20, 'click_device', By.XPATH, '//tr[contains(@class, "ListItem-module_row")]['+str(i + 1)+']//span[contains(@id, "download_and_transfer_list")]')
+      click_select(20, 'click_download', By.XPATH, '//tr[contains(@class, "ListItem-module_row")]['+str(i + 1)+']//div[contains(@id, "DOWNLOAD_AND_TRANSFER_ACTION")]/span[text()="Download"]')
+      click_select(20, 'notification_close', By.ID, 'notification-close')
 
+if __name__ == '__main__':
+  main()
