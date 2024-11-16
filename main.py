fa28ce331c3cddcd14003fc880b32941a5694f27
frustratedcat <brownjared205@gmail.com>
2024-11-16 18:54:54 -0500

    Clean things up a bit

--- main.py
+++ main.py
@@ -2,15 +2,22 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import creds
+import os
 
-# Connect
+# Clear Screen
+def clear_screen():
+  os.system('cls' if os.name == 'nt' else 'clear')
+
+# Start up and connect
+clear_screen()
+print('Starting up\n')
 driver = webdriver.Firefox()
 driver.get("https://wwww.amazon.com")
 
 # Click items
-def click_select(time, var, type, name):
+def click_select(time, type, name):
   driver.implicitly_wait(time)
-  var = driver.find_element(type, name).click()
+  driver.find_element(type, name).click()
 
 # Input items
 def input_select(time, var, type, name, file):
@@ -20,7 +27,7 @@ def input_select(time, var, type, name, file):
 
 # Login function
 def login():
-  click_select(20, 'account_list', By.ID, 'nav-link-accountList')
+  click_select(20, By.ID, 'nav-link-accountList')
   input_select(20, 'input_email', By.ID, 'ap_email', creds.get_username())
   input_select(20, 'input_password', By.ID, 'ap_password', creds.get_password())
   # Check for 2FA
@@ -29,9 +36,9 @@ def login():
 
 # Navigation function
 def nav_to_content_library():
-  click_select(100, 'account_list', By.ID, 'nav-link-accountList')
+  click_select(100, By.ID, 'nav-link-accountList')
   input_select(5, 'content_library', By.LINK_TEXT, 'Content Library', None)
-  click_select(5, 'books', By.ID, 'content-ownership-books')
+  click_select(5, By.ID, 'content-ownership-books')
 
 # Download items
 def download_items():
@@ -41,7 +48,7 @@ def download_items():
   # Pages loop
   for num in range(0, int(last_page)):
     # Next Page
-    click_select(20, 'page', By.XPATH, '//div[contains(@id, "pagination")]//a[contains(@id, "page-' + str(num + 1) + '")]')
+    click_select(20, By.XPATH, '//div[contains(@id, "pagination")]//a[contains(@id, "page-' + str(num + 1) + '")]')
 
     # Get total number of books on first page for looping
     all_books = driver.find_elements(By.XPATH, '//tr[contains(@class, "ListItem-module_row")]')
@@ -49,23 +56,25 @@ def download_items():
     # Loop items
     for i in range(len(all_books)):
       # Click dropdown
-      click_select(20, 'drop_down', By.XPATH, '//tr[contains(@class, "ListItem-module_row")][' + str(i + 1) + ']//div[contains(@id, "dd_title")]')
+      click_select(20, By.XPATH, '//tr[contains(@class, "ListItem-module_row")][' + str(i + 1) + ']//div[contains(@id, "dd_title")]')
 
       # Click transfer item
-      click_select(20, 'transfer_click', By.XPATH, '//tr[contains(@class, "ListItem-module_row")][' + str(i + 1) + ']//div[contains(@id, "DOWNLOAD_AND_TRANSFER")]')
+      click_select(20, By.XPATH, '//tr[contains(@class, "ListItem-module_row")][' + str(i + 1) + ']//div[contains(@id, "DOWNLOAD_AND_TRANSFER")]')
 
       # Choose device
-      click_select(20, 'click_device', By.XPATH, '//tr[contains(@class, "ListItem-module_row")][' + str(i + 1) + ']//span[contains(@id, "download_and_transfer_list")]')
+      click_select(20, By.XPATH, '//tr[contains(@class, "ListItem-module_row")][' + str(i + 1) + ']//span[contains(@id, "download_and_transfer_list")]')
 
       #Click download
-      click_select(20, 'click_download', By.XPATH, '//tr[contains(@class, "ListItem-module_row")][' + str(i + 1) + ']//div[contains(@id, "DOWNLOAD_AND_TRANSFER_ACTION")]/span[text()="Download"]')
+      click_select(20, By.XPATH, '//tr[contains(@class, "ListItem-module_row")][' + str(i + 1) + ']//div[contains(@id, "DOWNLOAD_AND_TRANSFER_ACTION")]/span[text()="Download"]')
 
       # Close the download notification
-      click_select(20, 'notification_close', By.ID, 'notification-close')
+      click_select(20, By.ID, 'notification-close')
 
 def main():
   # Log in
+  clear_screen()
   login()
+  clear_screen()
 
   # Navigate to content library
   nav_to_content_library()
