767c528c26e69cb0f857df43c4425a2c07a42342
frustratedcat <brownjared205@gmail.com>
2024-11-21 19:42:54 -0500

    Strip whitespace

--- creds.py
+++ creds.py
@@ -2,12 +2,12 @@ from getpass import getpass
 
 def get_username():
     username = input('Please enter your username\n>')
-    return username
+    return username.strip()
 
 def get_password():
     password = getpass('Please enter your password\n>')
-    return password
+    return password.strip()
 
 def get_two_factor():
     two_factor = input('Please enter your 2FA\n>')
-    return two_factor
+    return two_factor.strip()
